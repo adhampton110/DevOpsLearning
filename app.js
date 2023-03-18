463aff8028d6d15807c3c9ee3fdd04e35b3c3e96
adhampton110 <anthony.hampton1235@gmail.com>
2023-03-17 23:48:39 -0500

    Changed File Structure + Implemented Express
    - Added `express` as a dependency
    - `app.js` is the root file upon execution. This file initializes express and defines routes for the application.
    - Added skeleton content for `home.html`

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(express.static("public"));
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"views","home.html"));
+});
+
+app.get("/infrastructure_as_code", (req,res) => {
+    res.sendFile(path.join(__dirname,"views","infrastructure_as_code.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Application listening at localhost:${port}`);
+})
