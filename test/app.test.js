34ec3eb62571e679322c75a107134353bc64f8f6
adhampton110 <anthony.hampton1235@gmail.com>
2023-03-22 04:30:48 -0500

    Added Route Test For CI Overview view

--- test/app.test.js
+++ test/app.test.js
@@ -53,6 +53,18 @@ describe('GET /infrastructure_as_code', () => {
     });
 });
 
+describe('GET /continuous_integration/ci_overview', () => {
+  test('should respond with status code 200', async () => {
+      const response = await request(server).get('/continuous_integration/ci_overview');
+      expect(response.statusCode).toBe(200);
+  });
+
+  test('should render infrastructure_as_code view', async () => {
+      const response = await request(server).get('/continuous_integration/ci_overview');
+      expect(response.text).toContain('key-concepts-in-continuous-integration');
+  });
+});
+
 describe('GET /invalid-url', () => {
     test('should respond with status code 404', async () => {
         const response = await request(server).get('/invalid-url');
